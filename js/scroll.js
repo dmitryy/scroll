9cab8f59056aebd7d7bb83ac6f379be757482a16
dmitryy <dmitry@ruswizards.com>
2015-10-21 17:20:26 +0300

    more options

--- js/scroll.js
+++ js/scroll.js
@@ -7,28 +7,44 @@ for (var i = 0; i < count; i++) {
 }
 
 $(function () {
+    $('#btnClear').click(function () {
+        $('#test').empty();
+        $('#time').text('');
+    });
     $('#btnDraw100').click(function () {
+        var time = Date.now();
         $('#test').empty();
         for (var i = 0; i < 100; i++) {
             $('#test').append('<div>' + items[i].Name + '</div>');
         }
+        var diff = (Date.now() - time) / 1000;
+        $('#time').text('Took: ' + diff + ' seconds');
     });
     $('#btnDraw1k').click(function () {
+        var time = Date.now();
         $('#test').empty();
         for (var i = 0; i < 1000; i++) {
             $('#test').append('<div>' + items[i].Name + '</div>');
         }
+        var diff = (Date.now() - time) / 1000;
+        $('#time').text('Took: ' + diff + ' seconds');
     });
     $('#btnDraw10k').click(function () {
+        var time = Date.now();
         $('#test').empty();
         for (var i = 0; i < 10000; i++) {
             $('#test').append('<div>' + items[i].Name + '</div>');
         }
+        var diff = (Date.now() - time) / 1000;
+        $('#time').text('Took: ' + diff + ' seconds');
     });
     $('#btnDraw100k').click(function () {
+        var time = Date.now();
         $('#test').empty();
         for (var i = 0; i < 100000; i++) {
             $('#test').append('<div>' + items[i].Name + '</div>');
         }
+        var diff = (Date.now() - time) / 1000;
+        $('#time').text('Took: ' + diff + ' seconds');
     });
 });
